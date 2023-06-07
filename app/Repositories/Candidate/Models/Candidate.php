bac6c272fb74a81795cf8b6e72b65665c62026fe
redikawest <redikawest@gmail.com>
2023-06-07 20:00:17 +0800

    search candidate

--- app/Repositories/Candidate/Models/Candidate.php
+++ app/Repositories/Candidate/Models/Candidate.php
@@ -23,30 +23,6 @@ class Candidate extends Model
     {
         return $query->where(function ($query) use ($request) {
 
-            if ($request->has('partner') && $request->partner != '') {
-                $query->where('partnerId', $request->partner);
-            }
-
-            if ($request->has('account') && $request->account != '') {
-                $query->where('accountId', $request->account);
-            }
-
-            if ($request->has('gender') && $request->gender != '') {
-                $query->where('gender', $request->gender);
-            }
-
-            if ($request->has('device') && $request->device != '') {
-                $query->where('deviceData', 'LIKE', "%$request->device%");
-            }
-
-            if ($request->has('fromAge') && $request->fromAge != '') {
-                $query->whereBetween('age', [$request->fromAge, $request->toAge]);
-            }
-
-            if ($request->has('isSubscribe') && $request->isSubscribe != '') {
-                $query->where('isTickBox', true);
-            }
-
             if ($request->has('search') && strlen($request->search) > 1) {
                 $query->where(function ($search) use ($request) {
                     $search->where("full_name", "LIKE", "%$request->search%")
